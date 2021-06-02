71650250b362abb332cef23a1034f2d629e77ada
Armaan Swan <Swanarmaan@gmail.com>
2021-06-02 14:36:12 +0530

    Added Basic Styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import * as IoIcons from 'react-icons/io5';
+
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <main>
+        <section className="home">
+          <h1>Logistics Services Across <span className="text-primary">Canada</span></h1>
+        </section>
+      </main>
+    </>
   );
 }
 
